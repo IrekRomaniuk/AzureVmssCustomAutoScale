7511f84a0ff382e977fcc353cb85cfbcbb5bad69
zivst2016 <zivst2016@ZIVVS15SERVER>
2017-02-26 19:06:11 +0000

    Create ServiceBusLoader including ServiceBus SDK

--- vmssAutoScaleConsole/Program.cs
+++ vmssAutoScaleConsole/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using vmssAutoScale.BL;
 using vmssAutoScale.SqlLoadWatcher;
+using vmssAutoScale.ServiceBusWatcher;
 
 namespace vmssAutoScaleConsole
 {
@@ -13,17 +14,69 @@ namespace vmssAutoScaleConsole
     {
         static void Main(string[] args)
         {
+            Trace.WriteLine("****************************");
+            Trace.WriteLine("Welcom To VMSS Auto Scaleset");
+            Trace.WriteLine("****************************");
+            Trace.WriteLine("\n\n");
+
             while (!Console.KeyAvailable)
             {
-                SqlLoadWatcher sqlLoadWatcher = new SqlLoadWatcher();
-                AutoScaler autoScaler = new AutoScaler(sqlLoadWatcher);
-                autoScaler.TraceEvent += AutoScaler_TraceEvent;
-                Task t = autoScaler.AutoScale();
+                Console.WriteLine("Please select the Trriger interfacet:");
+                Console.WriteLine("1. SQL");
+                Console.WriteLine("2. Service Bus");
 
-                t.Wait();
-                Trace.WriteLine("Pausing for one minute");
-                Task.Delay(60000).Wait();
+                string strOption = Console.ReadLine();
+
+                switch (strOption)
+                {
+                    case "1":
+                        {
+                            SQLLoader();
+                        }
+                        break;
+                    case "2":
+                        {
+                            ServiceBusLoader();
+                        }
+                        break;
+                    default:
+                        {
+
+                        }
+                        break;
+                }                
             }
+        }
+
+        private static void SQLLoader()
+        {
+            Console.WriteLine("SQLLoader");
+
+            SqlLoadWatcher sqlLoadWatcher = new SqlLoadWatcher();
+            AutoScaler autoScaler = new AutoScaler(sqlLoadWatcher);
+            autoScaler.TraceEvent += AutoScaler_TraceEvent;
+            Task t = autoScaler.AutoScale();
+
+            t.Wait();
+            Trace.WriteLine("Pausing for one minute");
+            Task.Delay(60000).Wait();
+        }
+
+        private static void ServiceBusLoader()
+        {
+            Console.WriteLine("ServiceBusLoader");
+
+            ServiceBusWatcher MyServiceBusWatcher = new ServiceBusWatcher();
+            //MyServiceBusWatcher.GetCurrentLoad();
+            /*
+            SqlLoadWatcher sqlLoadWatcher = new SqlLoadWatcher();*/
+            AutoScaler autoScaler = new AutoScaler(MyServiceBusWatcher);
+            autoScaler.TraceEvent += AutoScaler_TraceEvent;
+            Task t = autoScaler.AutoScale();
+
+            t.Wait();
+            Trace.WriteLine("Pausing for one minute");
+            Task.Delay(60000).Wait();
         }
 
         private static void AutoScaler_TraceEvent(object sender, string message)
